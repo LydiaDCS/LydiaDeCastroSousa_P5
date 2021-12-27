d2682137dd3a67b8cce401f549e4fcd6e1516d1a
Lydia De Castro Sousa <lydiadecastrosousa@gmail.com>
2021-12-27 16:56:53 +0100

    page d'accueil produits ok

---
+++ front/js/script.js
@@ -0,0 +1,27 @@
+// J'initialise la variable produits de type Array qui contiendra mon api 
+let products = [];
+
+// Je récupére mes produits depuis mon API
+async function fetchApi() {
+    await fetch("http://localhost:3000/api/products")
+        .then((res) => res.json())
+        .then((data) => (products = data));
+}
+console.log(products);
+
+//Je crée une fonction avec une boucle pour afficher mes vignettes 
+async function canapDisplay() {
+    await fetchApi();
+    let items = document.getElementById("items");
+    for (let i = 0; i < products.length; i++) {
+        items.innerHTML +=
+            `<a href="./product.html?id=${products[i]._id}">
+              <article>
+                <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+                <h3 class="productName">${products[i].name}</h3>
+                <p class="productDescription">${products[i].description}</p>
+              </article>
+            </a>`;
+    }
+}
+canapDisplay();
